a7f7307cdfaeb14f9dde4e85d6daa3667422faeb
mdblj <thinkjin6@gmail.com>
2023-11-30 16:13:25 +0900

    ccip

--- test/integration/router/mainnet/TestRouter.t.sol
+++ test/integration/router/mainnet/TestRouter.t.sol
@@ -2,7 +2,6 @@
 pragma solidity 0.8.20;
 
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { IERC4626 } from "@openzeppelin/contracts/interfaces/IERC4626.sol";
 
 import { IAddressesProvider } from "contracts/interfaces/IAddressesProvider.sol";
 import { IRouter } from "contracts/interfaces/IRouter.sol";
